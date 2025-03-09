59019decc02027db57be1460b0fed1aafeca25f2
jungmin <jungmin.na@cj.net>
2025-03-09 22:00:16 +0900

    Add local images for AS case studies

--- app/services/cases/page.tsx
+++ app/services/cases/page.tsx
@@ -20,42 +20,42 @@ import { FaTools, FaBuilding } from 'react-icons/fa';
 const INSTALLATION_CASES = [
   {
     title: "래미안 아파트 CCTV 교체",
-    image: "https://images.pexels.com/photos/1643456/pexels-photo-1643456.jpeg",
+    image: "/images/as/img1.jpeg",
     type: "아파트",
     description: "노후화된 CCTV 200대 교체 및 신규 설치",
     details: "지하주차장 사각지대 해소, Full HD 고화질 카메라 교체"
   },
   {
     title: "자이 아파트 통합 관제",
-    image: "https://images.pexels.com/photos/3205567/pexels-photo-3205567.jpeg",
+    image: "/images/as/img2.jpeg",
     type: "아파트",
     description: "통합관제시스템 구축 및 설치",
     details: "AI 기반 지능형 감시 시스템 도입"
   },
   {
     title: "푸르지오 시티 보안강화",
-    image: "https://images.pexels.com/photos/430208/pexels-photo-430208.jpeg",
+    image: "/images/as/img3.jpeg",
     type: "오피스텔",
     description: "출입구 및 주차장 보안 강화",
     details: "차량번호 인식 시스템 및 얼굴인식 시스템 설치"
   },
   {
     title: "힐스테이트 CCTV 증설",
-    image: "https://images.pexels.com/photos/1854296/pexels-photo-1854296.jpeg",
+    image: "/images/as/img4.jpeg",
     type: "아파트",
     description: "단지 내 사각지대 CCTV 증설",
     details: "놀이터, 정원 등 안전 취약구역 중점 보강"
   },
   {
     title: "더샵 스마트시티",
-    image: "https://images.pexels.com/photos/139762/pexels-photo-139762.jpeg",
+    image: "/images/as/img5.jpeg",
     type: "주상복합",
     description: "스마트시티 통합 보안 시스템",
     details: "IoT 기반 스마트 보안 시스템 구축"
   },
   {
     title: "SK VIEW 보안시스템",
-    image: "https://images.pexels.com/photos/1619870/pexels-photo-1619870.jpeg",
+    image: "/images/as/img6.jpeg",
     type: "아파트",
     description: "단지 전체 보안시스템 고도화",
     details: "4K Ultra HD 카메라 설치 및 나이트비전 시스템 도입"
